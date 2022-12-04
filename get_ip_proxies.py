d3e1187396ac2a44683541143f3190668c91fad4
Atom <dateatom@gmail.com>
2022-12-04 11:30:27 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   get_ip_proxies.py

--- get_ip_proxies.py
+++ get_ip_proxies.py
@@ -7,7 +7,7 @@ from pandas import DataFrame
 base_url = 'https://www.kuaidaili.com/free/inha/'  # +page num
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
-    'Cookie': 'Hm_lpvt_7ed65b1cc4b810e9fd37959c9bb51b31=1668567145; Hm_lvt_7ed65b1cc4b810e9fd37959c9bb51b31=1668566130; _ga=GA1.2.1664604482.1668566130; _gid=GA1.2.711564170.1668566130; _gcl_au=1.1.2062328034.1668566131; channelid=0; sid=1668564531959599'
+    # 'Cookie': 'Hm_lpvt_7ed65b1cc4b810e9fd37959c9bb51b31=1668567145; Hm_lvt_7ed65b1cc4b810e9fd37959c9bb51b31=1668566130; _ga=GA1.2.1664604482.1668566130; _gid=GA1.2.711564170.1668566130; _gcl_au=1.1.2062328034.1668566131; channelid=0; sid=1668564531959599'
 }
 ip_pool_info = []
 page = 1
